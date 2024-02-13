4b77c86099c838d900ea8a9170054e9f729c3bbd
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-02-13 22:24:38 +0100

    got the base down

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// If image is invalid
+// const img = ('img');
+
+// img.addEventListener('error', function handleError() {
+//   const defaultImage =
+//     'assets/placeholder.jpg';
+//   img.src = defaultImage;
+//   img.alt = 'default';
+// }); 
